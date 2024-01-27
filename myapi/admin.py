5d33186705dd5c5a02c5302e86c5e931e6a7c146
ikrame <ikrameeidrissi@gmail.com>
2024-01-27 06:30:38 +0000

    Add_order APi

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,1 +1,11 @@
+from django.contrib import admin
 
+from myapi.models import Order
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date_entree', 'date_livraison', 'note_livreur', 'note_gestionnaire', 'nom_client', 'tel_client', 'region', 'adresse', 'nom_produit', 'unite', 'prix_propose', 'frais_livraison', 'gains', 'etat_livraison', 'soldee', 'date_added', 'url_order')
+
+
+# Enregistrez les modèles dans l'interface d'administration
+admin.site.register(Order, OrderAdmin)
--- myapi/urls.py
+++ myapi/urls.py
@@ -1,10 +1,10 @@
 # Dans myapp/urls.py
-from msilib import add_data
+
 
 from django.urls import path
-from .views import get_orders
+from .views import get_orders, add_order
 
 urlpatterns = [
     path('api/orders/', get_orders, name='get_orders'),
-    path('add_data/', add_data, name='add_data'),
+    path('add_data/', add_order, name='add_data'),
 ]
